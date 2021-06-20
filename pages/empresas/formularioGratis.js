be7dec8ba39ba6212d588c7c59dba6fdb145175d
Brianidle <ddbrayan10@hotmail.com>
2021-06-19 21:00:20 -0300

    New page created on the path: '/empresas/formularioGratis'. In this page the user can publish a JobAd.

---
+++ pages/empresas/formularioGratis.js
@@ -0,0 +1,291 @@
+import dynamic from "next/dynamic";
+import styled from "styled-components";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const CKEditor5 = dynamic(() => import("../../src/components/CKEditor5"), {
+  ssr: false,
+});
+
+const Header = styled.div`
+  text-align: center;
+`;
+
+const PublishJobAdDiv = styled.div`
+  width: 100%;
+  max-width: 920px;
+  margin: 40px auto;
+  background-color: white;
+`;
+
+const PublishJobAdContent = styled.div`
+  padding: 35px 20px;
+`;
+
+const PublishAFreeAdTitle = styled.h1`
+  font-weight: 700;
+  line-height: 30px;
+  font-size: 22px;
+  color: #535151;
+  margin: 0px;
+`;
+
+const FreeAdsAvailable = styled.span`
+  font-size: 14px;
+  color: #212121;
+`;
+
+const FormContainer = styled.form`
+  margin-top: 20px;
+`;
+
+const InputDescription = styled.label`
+  display: inline-block;
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 10px;
+  border-radius: 3px;
+  border: 1px solid #e5e5e5;
+  height: 35px;
+  font-size: 14px;
+
+  :focus {
+    box-shadow: inset 0 0 5px #66afe9;
+  }
+`;
+
+const InputsGrid = styled.div`
+  display: grid;
+  grid-template-columns: calc(50% - 10px) calc(50% - 10px);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 100%;
+    grid-column-gap: unset;
+  }
+`;
+
+const PublishButton = styled.button`
+  width: 100%;
+  max-width: 220px;
+  color: #fff;
+  background-color: #ff652b;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 50%);
+  border: none;
+  cursor: pointer;
+  font-family: "Open Sans", sans-serif;
+  margin-bottom: 0;
+  font-weight: 600;
+  padding: 10px 25px;
+  font-size: 14px;
+  text-transform: uppercase;
+  border-radius: 2px;
+  margin-top: 30px;
+`;
+
+const FormularioGratis = () => {
+  const [inputValues, setInputValues] = useState({
+    title: "",
+    description: "",
+    area: "",
+    subArea: "",
+    country: "Argentina",
+    state: "",
+    city: "",
+    address: "Dirección 1",
+    salary: "",
+    modality: "",
+    companyName: "",
+  });
+
+  const onChange = (event) => {
+    setInputValues({ ...inputValues, [event.target.name]: event.target.value });
+  };
+
+  const onChangeCKEditor = (ckeditorData) => {
+    setInputValues({ ...inputValues, [ckeditorData.name]: ckeditorData.data });
+    console.log(inputValues);
+  };
+
+  const router = useRouter();
+
+  return (
+    <PublishJobAdDiv>
+      <PublishJobAdContent>
+        <Header>
+          <PublishAFreeAdTitle>PUBLICÁ UN AVISO GRATIS</PublishAFreeAdTitle>
+          <FreeAdsAvailable>Tenés 3 avisos gratis disponibles</FreeAdsAvailable>
+        </Header>
+        <FormContainer
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetch("http://localhost:4862/jobAd", {
+              method: "post",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(inputValues),
+            })
+              .then((response) => response.json())
+              .then((data) => console.log(data));
+            router.push("/");
+          }}
+        >
+          <div style={{ marginBottom: "20px" }}>
+            <InputDescription htmlFor="title">
+              Puesto / Título del aviso
+            </InputDescription>
+            <Input
+              id="title"
+              name="title"
+              onChange={onChange}
+              value={inputValues.title ?? ""}
+              required
+            ></Input>
+          </div>
+          <div style={{ marginBottom: "20px" }}>
+            <InputDescription htmlFor="description">
+              Descripción del puesto
+            </InputDescription>
+            <CKEditor5 name="description" onChange={onChangeCKEditor} />
+          </div>
+          <InputsGrid>
+            <div>
+              <InputDescription htmlFor="area">Área de empleo</InputDescription>
+              <Input
+                id="area"
+                name="area"
+                onChange={onChange}
+                value={inputValues.area ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="subArea">
+                Subárea de empleo
+              </InputDescription>
+              <div>
+                <Input
+                  id="subArea"
+                  name="subArea"
+                  onChange={onChange}
+                  value={inputValues.subArea ?? ""}
+                  required
+                ></Input>
+              </div>
+            </div>
+            <div>
+              <InputDescription htmlFor="country">País</InputDescription>
+              <Input
+                id="country"
+                name="country"
+                value={inputValues.country ?? ""}
+                disabled
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="state">Provincia</InputDescription>
+              <Input
+                id="state"
+                name="state"
+                onChange={onChange}
+                value={inputValues.state ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="city">Ciudad</InputDescription>
+              <Input
+                id="city"
+                name="city"
+                onChange={onChange}
+                value={inputValues.city ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="address">Dirección</InputDescription>
+              <Input
+                id="address"
+                name="address"
+                onChange={onChange}
+                value={inputValues.address ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="salary">Sueldo</InputDescription>
+              <Input
+                id="salary"
+                name="salary"
+                onChange={onChange}
+                value={inputValues.salary ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="jobType">
+                Tipo de Empleo
+              </InputDescription>
+              <Input
+                id="jobType"
+                name="jobType"
+                onChange={onChange}
+                value={inputValues.jobType ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="modality">
+                Modalidad de Trabajo
+              </InputDescription>
+              <Input
+                id="modality"
+                name="modality"
+                onChange={onChange}
+                value={inputValues.modality ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="companyName">
+                Nombre de la Empresa
+              </InputDescription>
+              <Input
+                id="companyName"
+                name="companyName"
+                onChange={onChange}
+                value={inputValues.companyName ?? ""}
+                required
+              ></Input>
+            </div>
+            <div>
+              <InputDescription htmlFor="urlCompanyLogo">
+                Url del Logo de la Empresa
+              </InputDescription>
+              <Input
+                id="urlCompanyLogo"
+                name="urlCompanyLogo"
+                onChange={onChange}
+                value={inputValues.urlCompanyLogo ?? ""}
+                required
+              ></Input>
+            </div>
+          </InputsGrid>
+          <div style={{ textAlign: "center" }}>
+            <PublishButton type="submit">PUBLICAR</PublishButton>
+          </div>
+        </FormContainer>
+      </PublishJobAdContent>
+    </PublishJobAdDiv>
+  );
+};
+
+export default FormularioGratis;
