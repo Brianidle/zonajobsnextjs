9be18b179de08f364e41217900f86f3dae72f9e6
Brianidle <ddbrayan10@hotmail.com>
2021-06-27 15:33:49 -0300

    The src/modules/ckeditor5 was replaced for a New custom ckeditor5 package added to the project. This package have a word counter plugin on it.

--- src/modules/ckeditor5/src/ckeditor.js
+++ src/modules/ckeditor5/src/ckeditor.js
@@ -4,34 +4,30 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Alignment,
-	Autoformat,
 	Bold,
 	Essentials,
-	Indent,
 	Italic,
 	List,
 	Paragraph,
 	PasteFromOffice,
 	Strikethrough,
-	TextTransformation,
-	Underline
+	Underline,
+	WordCount
 ];
 
 export default Editor;
