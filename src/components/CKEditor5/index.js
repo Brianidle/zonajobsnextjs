a6a51d12d3f585c0126db4caf5201522765f80af
Brianidle <ddbrayan10@hotmail.com>
2021-06-27 16:43:26 -0300

    CKEditor5 component modified. A label was added to show the number of characters and the max number of characters. When the maximum number of characters is surpassed a error label is shown and the component is covered by a red border. The component receive a function which tells its parent component the number of characters in the editor.

--- src/components/CKEditor5/index.js
+++ src/components/CKEditor5/index.js
@@ -1,8 +1,29 @@
 import { useRef, useState, useEffect } from "react";
+import styled from "styled-components";
 
-const CKEditorCustom = ({ name, onChange }) => {
+const CKEditorDiv = styled.div`
+  position: relative;
+  text-align: right;
+  ${props => props.maxNumCharactersSurpassed ? "border: 1px solid red" : ""};
+`;
+
+const NumberOfWords = styled.label`
+  position: absolute;
+  bottom: 5px;
+  right: 10px;
+  font-size: 14px;
+`;
+
+const ErrorMessage = styled.label`
+  color: #ff4040;
+  font-size: 13px;
+`;
+
+const CKEditorCustom = ({ name, onChange, maxNumberOfCharacters, setNumberOfCharactersEditor }) => {
   const editorRef = useRef();
   const [editorLoaded, setEditorLoaded] = useState(false);
+  const [numberOfWords, setNumberOfWords] = useState(0);
+  const [maxNumberOfCharactersSurpassed, setMaxNumberOfCharactersSurpassed] = useState(false);
   const { CKEditor, ClassicEditor } = editorRef.current || {};
 
   useEffect(() => {
@@ -28,26 +49,38 @@ const CKEditorCustom = ({ name, onChange }) => {
       "|",
       "undo",
       "redo",
-    ],
+    ], wordCount: {
+      onUpdate: stats => {
+        let numberCharacters = stats.characters;
+        setNumberOfWords(numberCharacters);
+        setNumberOfCharactersEditor(numberCharacters);
+        setMaxNumberOfCharactersSurpassed(numberCharacters > maxNumberOfCharacters);
+      }
+    }
   };
 
   return (
     <>
-      {editorLoaded ? (
-        <CKEditor
-          editor={ClassicEditor}
-          config={editorConfiguration}
-          data={data}
-          name={name}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            setData(data);
-            onChange({ name, data });
-          }}
-        />
-      ) : (
-        <p>Cargando...</p>
-      )}
+      <CKEditorDiv maxNumCharactersSurpassed={maxNumberOfCharactersSurpassed}>
+        {editorLoaded ? (
+          <>
+            <CKEditor
+              editor={ClassicEditor}
+              config={editorConfiguration}
+              data={data}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                setData(data);
+                onChange({ name, data });
+              }}
+            />
+            <NumberOfWords>{numberOfWords}/{maxNumberOfCharacters}</NumberOfWords>
+          </>
+        ) : (
+          <></>
+        )}
+      </CKEditorDiv>
+      {maxNumberOfCharactersSurpassed && <ErrorMessage>Superaste el maximo de caracteres permitidos</ErrorMessage>}
     </>
   );
 };
